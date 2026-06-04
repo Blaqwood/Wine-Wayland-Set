dd4a9185b62bc85d4d6949c0544c9e0f430cc0a2
Jonathan Blackwood <68885343+Blaqwood@users.noreply.github.com>
2026-06-04 09:59:50 -0500

    Add third option for both X11 and Wayland

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 echo "1) Wayland only"
 echo "2) X11 only"
+echo "3) Both"
 read -p "Select a display driver for wine: " num
 
 # adds registry value with selected option
@@ -7,8 +8,10 @@ if [ $num -eq 1 ]; then
 	wine reg add 'HKEY_CURRENT_USER\Software\Wine\Drivers' /v Graphics /d 'wayland'
 elif [ $num -eq 2 ]; then
 	wine reg add 'HKEY_CURRENT_USER\Software\Wine\Drivers' /v Graphics /d 'x11'
+elif [ $num -eq 3 ]; then
+	wine reg add 'HKEY_CURRENT_USER\Software\Wine\Drivers' /v Graphics /d 'wayland,x11'
 else
-	echo "Select 1-2, You selected $num"
+	echo "Select 1-3, You selected $num"
 fi
 
 echo "Exiting..."
