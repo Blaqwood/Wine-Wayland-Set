c2d4ab23eeda8ca0a62836b0da3efd50182683cf
Jonathan Blackwood <68885343+Blaqwood@users.noreply.github.com>
2026-06-04 10:06:39 -0500

    No need for extra user input
    Add /f to the wine command so that the user does not have to type yes in the prompt.

--- main.sh
+++ main.sh
@@ -6,11 +6,11 @@ read -p "Select a display driver for wine: " num
 
 # adds registry value with selected option
 if [ $num -eq 1 ]; then
-	wine reg add 'HKEY_CURRENT_USER\Software\Wine\Drivers' /v Graphics /d 'wayland'
+	wine reg add 'HKEY_CURRENT_USER\Software\Wine\Drivers' /v Graphics /d 'wayland' /f
 elif [ $num -eq 2 ]; then
-	wine reg add 'HKEY_CURRENT_USER\Software\Wine\Drivers' /v Graphics /d 'x11'
+	wine reg add 'HKEY_CURRENT_USER\Software\Wine\Drivers' /v Graphics /d 'x11' /f
 elif [ $num -eq 3 ]; then
-	wine reg add 'HKEY_CURRENT_USER\Software\Wine\Drivers' /v Graphics /d 'wayland,x11'
+	wine reg add 'HKEY_CURRENT_USER\Software\Wine\Drivers' /v Graphics /d 'wayland,x11' /f
 else
 	echo "Select 1-3, You selected $num"
 fi
