7e8df99c85c2ea6b46cab76ee7ee7addce000c40
Jonathan Blackwood <68885343+Blaqwood@users.noreply.github.com>
2026-06-04 14:50:39 -0500

    Customize wine prefix

--- main.sh
+++ main.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
 echo "1) Wayland only"
 echo "2) X11 only"
 echo "3) Both"
 read -p "Select a display driver for wine: " num
+read -p "Select a wine prefix (Leave blank for default): " prefix
+
+# set wine prefix
+if [ -z "$prefix" ]; then
+	export WINEPREFIX=~/.wine
+else
+	export WINEPREFIX=$(eval echo "$prefix")
+fi
+
+echo Using prefix $WINEPREFIX
 
 # adds registry value with selected option
 if [ $num -eq 1 ]; then
